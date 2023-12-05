5871eb705e6b00b99ea8119060828cc452296207
anExecutioner <pankajsoni7788@gmail.com>
2023-12-05 18:12:11 +0530

    1

--- naukri.py
+++ naukri.py
@@ -20,7 +20,8 @@ class naukri_update():
         profile = driver.find_element_by_xpath("//a[@href='/mnjuser/profile']")
         driver.execute_script("arguments[0].click();",profile)
         edit = "//div[@class='widgetHead']//span[text()='Resume headline']/following-sibling::span"
-        driver.execute_script("arguments[0].click();",edit)
+        x1 = driver.find_element_by_xpath(edit)
+        driver.execute_script("arguments[0].click();",x1)
         txt = driver.find_element_by_id("resumeHeadlineTxt")
         assert naukri_update.pre_text == txt.text, "Fail"
         txt.clear()
